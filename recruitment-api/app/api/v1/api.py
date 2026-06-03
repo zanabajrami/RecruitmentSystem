fd9cf512666e850d04ef177f59f4199a56c453d6
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-06-03 11:21:25 +0200

    chore: untrack cached python files

--- recruitment-api/app/api/v1/api.py
+++ recruitment-api/app/api/v1/api.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from app.api.v1 import auth
+from app.api.v1.endpoints import companies
 
 api_router = APIRouter()
+
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(companies.router, prefix="/companies", tags=["Companies"])
