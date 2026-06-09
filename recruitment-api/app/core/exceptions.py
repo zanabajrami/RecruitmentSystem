64bdb1bcfe88ec41d0edf2341037f8361d49180f
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-06-09 09:27:22 +0200

    remove warnings

--- recruitment-api/app/core/exceptions.py
+++ recruitment-api/app/core/exceptions.py
@@ -7,12 +7,12 @@ class CustomAPIException(HTTPException):
 
 class NotFoundException(CustomAPIException):
     def __init__(self, detail: str = "Resource not found"):
-        super().__init__(status_code=status_code.HTTP_404_NOT_FOUND, detail=detail, code="NOT_FOUND")
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail, code="NOT_FOUND")
 
 class BadRequestException(CustomAPIException):
     def __init__(self, detail: str = "Bad request"):
-        super().__init__(status_code=status_code.HTTP_400_BAD_REQUEST, detail=detail, code="BAD_REQUEST")
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail, code="BAD_REQUEST")
 
 class UnauthorizedException(CustomAPIException):
     def __init__(self, detail: str = "Could not validate credentials"):
-        super().__init__(status_code=status_code.HTTP_401_UNAUTHORIZED, detail=detail, code="UNAUTHORIZED")
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail, code="UNAUTHORIZED")
