63efb0957299b695b13005145e65ef7805f8c6fe
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2026-07-30 11:30:53 +0200

    refactor(schemas): update Notification schema with base and create models

--- recruitment-api/app/schemas/notification.py
+++ recruitment-api/app/schemas/notification.py
@@ -1,11 +1,19 @@
 from datetime import datetime
 from pydantic import BaseModel
 
-class NotificationResponse(BaseModel):
-    id: int
-    user_id: int
+
+class NotificationBase(BaseModel):
     title: str
     message: str
+
+
+class NotificationCreate(NotificationBase):
+    user_id: int
+
+
+class NotificationResponse(NotificationBase):
+    id: int
+    user_id: int
     is_read: bool
     created_at: datetime
 
